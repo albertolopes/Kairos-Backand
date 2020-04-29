491ce49d79dcdbe5813daf66cff2d630429da6bc
albertolopes <alberto_llopes@hotmail.com>
2020-04-29 18:04:25 -0300

    Update TarefasRepository.java

--- src/main/java/com/allos/pomodoro/repository/TarefasRepository.java
+++ src/main/java/com/allos/pomodoro/repository/TarefasRepository.java
@@ -2,6 +2,8 @@ package com.allos.pomodoro.repository;
 
 import com.allos.pomodoro.entity.Tarefas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefasRepository extends JpaRepository<Tarefas, Long> {
 }
