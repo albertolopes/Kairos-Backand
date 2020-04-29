119fd65ba96c74fa6475d4268e896c54b5285b51
albertolopes <alberto_llopes@hotmail.com>
2020-04-29 19:50:54 -0300

    Update Usuario.java

--- src/main/java/com/allos/pomodoro/entity/Usuario.java
+++ src/main/java/com/allos/pomodoro/entity/Usuario.java
@@ -17,7 +17,7 @@ public class Usuario implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
+    @Column(name = "ID_USUARIO")
     private Long id;
 
     @Column(name = "NOME")
